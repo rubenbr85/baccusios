bac4d81562e7ccf416e9057f0a7f072ff37ff6f6
Ruben Berreguero <rubenberreguero@MacBook-de-Ruben.local>
2013-08-29 17:48:10 +0200

    Initial Commit

---
+++ Baccus/RBRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RBRAppDelegate.h
+//  Baccus
+//
+//  Created by Ruben Berreguero on 29/08/13.
+//  Copyright (c) 2013 Ruben Berreguero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RBRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Baccus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Baccus
+//
+//  Created by Ruben Berreguero on 29/08/13.
+//  Copyright (c) 2013 Ruben Berreguero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RBRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RBRAppDelegate class]));
+    }
+}
